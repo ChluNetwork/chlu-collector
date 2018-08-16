26e3d1fbe3b7fce3e79c20ef8fe410aebaac6d20
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-08-16 14:42:52 +0200

    add CLI parameters for ChluDB configuration

--- src/bin.js
+++ src/bin.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 const path = require('path')
 const ChluIPFS = require('chlu-ipfs-support');
+const ChluSQLIndex = require('chlu-ipfs-support/src/modules/orbitdb/indexes/sql')
 const ChluCollector = require('./')
 const cli = require('commander');
 const package = require('../package.json');
@@ -28,6 +29,15 @@ async function start(options){
         directory: options.directory || path.join(process.env.HOME, '.chlu-collector'),
         ipfs: {
             remote: options.externalIpfs
+        },
+        OrbitDBIndex: ChluSQLIndex,
+        OrbitDBIndexOptions: {
+            dialect: options.postgres ? 'postgres' : null,
+            host: options.databaseHost,
+            port: options.databasePort,
+            database: options.databaseDb,
+            username: options.databaseUser,
+            password: options.databasePassword,
         },
         listen: options.listen,
         useCircuit: options.circuit || options.relay,
@@ -70,8 +80,8 @@ process.on('SIGINT', async function() {
 
 
 cli
-    .name('chlu-service-node')
-    .description('Reference implementation of the Chlu Service Node. http://chlu.io')
+    .name('chlu-collector')
+    .description('Reference implementation of the Chlu Collector. http://chlu.io')
     .version(package.version);
 
 cli
@@ -81,14 +91,21 @@ cli
     .option('-n, --network <network>', 'use a custom network instead of experimental')
     .option('-d, --directory <path>', 'where to store chlu data, defaults to ~/.chlu-collector')
     .option('--offline', 'signal other Chlu apps on your machine to ONLY connect to this service node and work offline')
-    .option('--bootstrap', 'connect to Chlu bootstrap nodes (not recommended right now)')
     // Blockchain
     .option('--btc <token>', 'turn on BTC Blockchain access using a Blockcypher API Token. Other systems will be supported in the future')
     .option('--btc-network <network>', 'choose the BTC network you want to connect to. Default is test3')
+    // ChluDB Options
+    .option('--postgres', 'connect to PostgreSQL database, uses SQLite otherwise')
+    .option('--database-host <s>', 'defaults to localhost')
+    .option('--database-port <s>', 'defatuls to the default postgresql port')
+    .option('--database-db <s>', 'defatuls to \'chlu\'')
+    .option('--database-user <s>', 'defatuls to \'chlu\'')
+    .option('--database-password <s>', 'defatuls to empty password', null, '')
     // IPFS/libp2p options
     .option('--listen', 'listen for incoming connections (not recommended right now)')
     .option('--no-rendezvous', 'disable usage of rendezvous servers (not recommended right now)')
     // TODO: reenable these when they are supported again
+//  .option('--bootstrap', 'connect to Chlu bootstrap nodes (not recommended right now)')
 //  .option('-e, --external-ipfs', 'connect to a running IPFS node at localhost:5001 instead of running IPFS internally')
 //  .option('-c, --circuit', 'enable libp2p circuit relay to use relays to connect to peers')
 //  .option('-r, --relay', 'act as libp2p relay to help nodes connect to each other (implicitly turns on --circuit)')
