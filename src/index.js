0bdd2e6817489fcd7d3466d672eafea7c088bdc3
Enrico Fasoli <enrico.fasoli1996@gmail.com>
2018-08-22 12:16:42 +0200

    improve DID and review Pinning. Add Tests

--- src/index.js
+++ src/index.js
@@ -11,16 +11,55 @@ class ChluCollector {
         this.handler = message => {
             return self.handleMessage(message);
         };
-        this.didPinner = async didId => {
+        this.didPinner = async (didId, multihash = null) => {
             try {
-                const { multihash } = await this.chluIpfs.orbitDb.getDID(didId)
                 if (multihash) {
+                    this.chluIpfs.logger.debug(`Pinning pre-validated DID ${didId} at ${multihash}`);
                     await this.pinner(multihash)
+                    this.chluIpfs.logger.debug(`Pinned pre-validated DID ${didId} at ${multihash}`);
                 } else {
-                    throw new Error(`Cannot Pin DID ${didId}: not found`)
+                    this.chluIpfs.logger.debug(`Fetching and validating DID ${didId}`);
+                    const { multihash } = await this.chluIpfs.orbitDb.getDID(didId)
+                    if (multihash) {
+                        this.chluIpfs.logger.debug(`Pinning DID ${didId} at ${multihash}`);
+                        await this.pinner(multihash)
+                        this.chluIpfs.logger.debug(`Pinned DID ${didId} at ${multihash}`);
+                    } else {
+                        throw new Error(`Cannot Pin DID ${didId}: not found`)
+                    }
                 }
             } catch (error) {
                 this.chluIpfs.logger.error(`Could not Pin DID ${didId} due to Error: ${error.message}`)
+            }
+        }
+        this.reviewPinner = async (multihash, reviewRecord = null, bitcoinNetwork = null, bitcoinTransactionHash = null) => {
+            this.chluIpfs.logger.info('Reading and Pinning ReviewRecord ' + multihash);
+            try {
+                if (reviewRecord && reviewRecord.resolved && isEmpty(reviewRecord.errors)) {
+                    this.chluIpfs.logger.debug('Pinning pre-validated ReviewRecord ' + multihash);
+                    await this.chluIpfs.pin(multihash)
+                    this.chluIpfs.logger.debug('Pinned pre-validated ReviewRecord ' + multihash);
+                } else {
+                    // Read review record first. This caches the content, the history, and throws if it's not valid
+                    this.chluIpfs.logger.debug('Reading and validating ReviewRecord ' + multihash);
+                    // TODO: this network checking logic should be moved somewhere else
+                    if(!isEmpty(bitcoinNetwork) && bitcoinNetwork !== this.chluIpfs.bitcoin.getNetwork()) {
+                        throw new Error(
+                            'Review Record ' + multihash + ' with txId ' + bitcoinTransactionHash
+                            + ' had bitcoin network ' + bitcoinNetwork
+                            + ' (expected ' + this.chluIpfs.bitcoin.getNetwork() + ')'
+                        );
+                    }
+                    await this.chluIpfs.readReviewRecord(multihash, {
+                        bitcoinTransactionHash: bitcoinTransactionHash
+                    });
+                    this.chluIpfs.logger.debug('Pinning validated ReviewRecord ' + multihash);
+                    await this.chluIpfs.pin(multihash);
+                    this.chluIpfs.logger.info('Validated and Pinned ReviewRecord data for ' + multihash);
+                }
+            } catch(exception){
+                this.chluIpfs.logger.error('Pinning failed due to Error: ' + exception.message);
+                console.log(exception)
             }
         }
         this.pinner = async multihash => {
@@ -41,18 +80,14 @@ class ChluCollector {
                 this.chluIpfs.logger.warn('Could not send Service Node message due to Error: ' + error.message);
             }
         };
-        this.replicatingNotifier = async address => {
-            try {
-                await this.chluIpfs.broadcast({
-                    type: this.chluIpfs.constants.eventTypes.replicating,
-                    address
-                });
-            } catch (error) {
-                this.chluIpfs.logger.warn('Could not send Service Node message due to Error: ' + error.message);
-            }
-        };
+        this.oplogEntryPinner = async multihash => {
+            this.chluIpfs.logger.debug(`Pinning OrbitDB Oplog Entry ${multihash}`)
+            await this.pinner(multihash)
+        }
         // Handle Chlu network messages
         this.chluIpfs.events.on('pubsub/message', this.handler);
+        // Pin Review Records
+        this.chluIpfs.events.on('discover/reviewrecord', this.reviewPinner)
         // Pin DIDs and public keys
         this.chluIpfs.events.on('discover/did', this.didPinner);
         this.chluIpfs.events.on('discover/did/customer', this.didPinner);
@@ -60,6 +95,9 @@ class ChluCollector {
         this.chluIpfs.events.on('discover/did/vendor', this.didPinner);
         this.chluIpfs.events.on('discover/did/marketplace', this.didPinner);
         this.chluIpfs.events.on('discover/keys/vendor-marketplace', this.pinner);
+        // Pin OrbitDB data
+        this.chluIpfs.events.on('db/load/progress', (address, hash) => this.oplogEntryPinner(hash))
+        this.chluIpfs.events.on('db/replicate/progress', (address, hash) => this.oplogEntryPinner(hash))
         // Send messages on replication
         this.chluIpfs.events.on('db/replicated', this.replicatedNotifier);
         this.chluIpfs.logger.debug('Started Chlu Collector')
@@ -79,28 +117,7 @@ class ChluCollector {
         let obj = message;
         // handle ReviewRecord: pin hash
         if (obj.type === this.chluIpfs.constants.eventTypes.wroteReviewRecord && typeof obj.multihash === 'string') {
-            this.chluIpfs.logger.info('Reading and Pinning ReviewRecord ' + obj.multihash);
-            try {
-                // Read review record first. This caches the content, the history, and throws if it's not valid
-                this.chluIpfs.logger.debug('Reading and validating ReviewRecord ' + obj.multihash);
-                // TODO: this network checking logic should be moved somewhere else
-                if(!isEmpty(obj.bitcoinNetwork) && obj.bitcoinNetwork !== this.chluIpfs.bitcoin.getNetwork()) {
-                    throw new Error(
-                        'Review Record ' + obj.multihash + ' with txId ' + obj.bitcoinTransactionHash
-                        + ' had bitcoin network ' + obj.bitcoinNetwork
-                        + ' (expected ' + this.chluIpfs.bitcoin.getNetwork() + ')'
-                    );
-                }
-                await this.chluIpfs.readReviewRecord(obj.multihash, {
-                    bitcoinTransactionHash: obj.bitcoinTransactionHash
-                });
-                this.chluIpfs.logger.debug('Pinning validated ReviewRecord ' + obj.multihash);
-                await this.chluIpfs.pin(obj.multihash);
-                this.chluIpfs.logger.info('Validated and Pinned ReviewRecord ' + obj.multihash);
-            } catch(exception){
-                this.chluIpfs.logger.error('Pinning failed due to Error: ' + exception.message);
-                console.log(exception)
-            }
+            await this.reviewPinner(obj.multihash, null, obj.bitcoinNetwork, obj.bitcoinTransactionHash)
         }
     }
 }
--- tests/collector.test.js
+++ tests/collector.test.js
@@ -2,31 +2,148 @@ const expect = require('chai').expect
 const sinon = require('sinon')
 const EventEmitter = require('events')
 const ChluIPFS = require('chlu-ipfs-support')
+const logger = require('chlu-ipfs-support/tests/utils/logger')
 const ChluCollector = require('../src')
 
 describe('Chlu Collector', () => {
     
     let chluIpfs, collector
 
-    beforeEach(() => {
+    const fakeDidMultihash = 'Qmdid'
+    const fakeMultihash = 'Qmdata'
+    const fakeDidId = 'did:chlu:fake'
+
+    beforeEach(async () => {
         chluIpfs = {
             events: new EventEmitter(),
+            logger: logger('Collector'),
             constants: ChluIPFS,
             orbitDb: {
-                getDID: sinon.stub().resolves()
+                getDID: sinon.stub().resolves({ multihash: fakeDidMultihash })
             },
             readReviewRecord: sinon.stub().resolves(),
-            pin: sinon.stub().resolves(),
-            broadcast: sinon.stub().resolves()
+            broadcast: sinon.stub().callsFake(async msg => chluIpfs.events.emit('mock/message-written', msg))
         }
+        chluIpfs.pin = sinon.stub().callsFake(async multihash => chluIpfs.events.emit('chlu-ipfs/pinned', multihash))
         collector = new ChluCollector(chluIpfs)
+        await collector.start()
     })
     
-    it('pins customer DIDs')
-    it('pins vendor DIDs')
-    it('pins marketplace DIDs')
-    it('pins VM Public Keys from PoPRs')
-    it('pins review records')
-    it('broadcasts replicated (done) event')
-    it('broadcasts replicating (in progress) event')
+    it('pins customer DIDs', done => {
+        expect(chluIpfs.orbitDb.getDID.called).to.be.false
+        expect(chluIpfs.pin.called).to.be.false
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.orbitDb.getDID.calledWith(fakeDidId)).to.be.true
+            expect(chluIpfs.pin.calledWith(fakeDidMultihash)).to.be.true
+            done()
+        })
+        chluIpfs.events.emit('discover/did/customer', fakeDidId)
+    })
+
+    it('pins vendor DIDs', done => {
+        expect(chluIpfs.orbitDb.getDID.called).to.be.false
+        expect(chluIpfs.pin.called).to.be.false
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.orbitDb.getDID.calledWith(fakeDidId)).to.be.true
+            expect(chluIpfs.pin.calledWith(fakeDidMultihash)).to.be.true
+            done()
+        })
+        chluIpfs.events.emit('discover/did/vendor', fakeDidId)
+    })
+
+    it('pins marketplace DIDs', done => {
+        expect(chluIpfs.orbitDb.getDID.called).to.be.false
+        expect(chluIpfs.pin.called).to.be.false
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.orbitDb.getDID.calledWith(fakeDidId)).to.be.true
+            expect(chluIpfs.pin.calledWith(fakeDidMultihash)).to.be.true
+            done()
+        })
+        chluIpfs.events.emit('discover/did/marketplace', fakeDidId)
+    })
+
+    it('pins other DIDs', done => {
+        expect(chluIpfs.orbitDb.getDID.called).to.be.false
+        expect(chluIpfs.pin.called).to.be.false
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.orbitDb.getDID.calledWith(fakeDidId)).to.be.true
+            expect(chluIpfs.pin.calledWith(fakeDidMultihash)).to.be.true
+            done()
+        })
+        chluIpfs.events.emit('discover/did', fakeDidId)
+    })
+
+    it('pins VM Public Keys from PoPRs', done => {
+        expect(chluIpfs.pin.called).to.be.false
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.pin.calledWith(fakeMultihash)).to.be.true
+            done()
+        })
+        chluIpfs.events.emit('discover/keys/vendor-marketplace', fakeMultihash)
+    })
+
+    it('pins pre-fetched review records from orbit-db', done => {
+        expect(chluIpfs.pin.called).to.be.false
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.pin.calledWith(fakeMultihash)).to.be.true
+            expect(chluIpfs.readReviewRecord.called).to.be.false
+            done()
+        })
+        chluIpfs.events.emit('discover/reviewrecord', fakeMultihash, { errors: [], resolved: true })
+    })
+
+    it('fetches and pins review records from pubsub', done => {
+        expect(chluIpfs.pin.called).to.be.false
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.pin.calledWith(fakeMultihash)).to.be.true
+            expect(chluIpfs.readReviewRecord.calledWith(fakeMultihash)).to.be.true
+            done()
+        })
+        chluIpfs.events.emit('pubsub/message', { multihash: fakeMultihash, type: ChluIPFS.eventTypes.wroteReviewRecord })
+    })
+
+    it('pins pre-fetched DIDs from orbit-db', done => {
+        expect(chluIpfs.pin.called).to.be.false
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.pin.calledWith(fakeDidMultihash)).to.be.true
+            expect(chluIpfs.orbitDb.getDID.called).to.be.false
+            done()
+        })
+        chluIpfs.events.emit('discover/did', fakeDidId, fakeDidMultihash, { id: fakeDidId })
+    })
+
+    it('broadcasts replicated (done) event', done => {
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.pin.calledWith(fakeDidMultihash)).to.be.true
+            expect(chluIpfs.orbitDb.getDID.called).to.be.false
+            done()
+        })
+        chluIpfs.events.emit('discover/did', fakeDidId, fakeDidMultihash, { id: fakeDidId })
+    })
+
+    it('pins oplog entries while loading', done => {
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.pin.calledWith(fakeMultihash)).to.be.true
+            done()
+        })
+        chluIpfs.events.emit('db/load/progress', null, fakeMultihash)
+    })
+
+    it('pins oplog entries while replicating', done => {
+        chluIpfs.events.on('chlu-ipfs/pinned', () => {
+            expect(chluIpfs.pin.calledWith(fakeMultihash)).to.be.true
+            done()
+        })
+        chluIpfs.events.emit('db/replicate/progress', null, fakeMultihash)
+    })
+
+    it('broadcasts replicated notification', done => {
+        const address = 'mydb'
+        chluIpfs.events.on('mock/message-written', msg => {
+            expect(msg.type).to.equal(ChluIPFS.eventTypes.replicated)
+            expect(msg.address).to.equal(address)
+            done()
+        })
+        chluIpfs.events.emit('db/replicated', address)
+    })
 })
